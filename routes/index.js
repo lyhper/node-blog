b5e57b29425c61cbec1c04f3dd8f8403c31e2c63
liuyuhang <lyhper@gmail.com>
2016-03-02 22:39:24 +0800

    complete post

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Post = require('../models/post.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
-    title: '主页',
-    user: req.session.user,
-    success: req.flash('success').toString(),
-    error: req.flash('error').toString()
+  Post.get(null, function(err, posts){
+    if(err){
+      posts = [];
+    }
+    res.render('index', {
+      title: '主页',
+      user: req.session.user,
+      posts: posts,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
   });
 });
 
--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var permission = require('./permission.js');
+var Post = require('../models/post.js');
 
 router.get('/', permission.checkLogin);
-router.get('/',function(req,res,next){
-	console.log('进');
-	res.render('post',{title:'发表'});
+router.get('/',function(req,res){
+	res.render('post',{
+		title:'发表',
+		user: req.session.user,
+		success: req.flash('success').toString(),
+		error: req.flash('error').toString()
+	});
 });
 router.post('/', permission.checkLogin);
-router.post('/',function(req,res,next){
+router.post('/',function(req,res){
+	var user = req.session.user;
+	var post = new Post(user.name, req.body.title, req.body.post);
+	post.save(function(err){
+		if(err){
+			req.flash('error',err);
+			return res.redirect('back');
+		}
+		req.flash('success','发布成功！');
+		res.redirect('/');
+	})
 });
 
 module.exports = router;
